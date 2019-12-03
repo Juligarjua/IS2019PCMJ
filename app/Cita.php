5e0e06bc566f5fe527efd977105c6f561a41c7dd
pablo <pabmarsan6@alum.us.es>
2019-12-03 18:13:05 +0100

    Modificado clases de tratamiento y citas

--- app/Cita.php
+++ app/Cita.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cita extends Model
 {
-    protected $fillable = ['fecha_hora','lugar', 'medico_id', 'paciente_id'];
+    protected $fillable = ['fecha_hora','lugar', 'medico_id', 'paciente_id','tratamiento_id'];
 
     public function medico()
     {
@@ -16,6 +16,11 @@ class Cita extends Model
     public function paciente()
     {
         return $this->belongsTo('App\Paciente');
+    }
+
+    public function tratamiento()
+    {
+        return $this->belongsToMany('App\Tratamiento')-> using('database\migrations');
     }
 
 
